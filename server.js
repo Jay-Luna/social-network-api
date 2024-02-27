0a0e81694757c44e3ad22717811d4dd801e887a6
Jay Luna <Lunajt@gmail.com>
2024-02-27 12:16:47 -0500

    Connect mongodb & add inital user data

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const db = require('./config/connection');
 const routes = require('./controllers');
 
 const app = express();
@@ -7,12 +7,10 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
 app.use(routes);
 
-app.listen(PORT, () =>
-    console.log(
-        `Server running on port ${PORT}.`
-    )
-);
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}.`);
+    });
+});
